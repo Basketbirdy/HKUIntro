6bed48a43ebbbe241c6664c7d66fa810381a9d15
Basketbirdy <danielmb0045@gmail.com>
2023-08-30 12:42:45 +0200

    v0.0.1
    - Update

--- HKUIntroJam/Assets/Scripts/Player/PlayerMovement.cs
+++ HKUIntroJam/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [TextArea(3,5)][SerializeField] string note;
+    [TextArea(3, 5)][SerializeField] string note;
 
     [Header("References")]
     [SerializeField] Collider2D coll;
@@ -16,34 +16,99 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float jumpheight = 10f;
 
+    [Header("Jumping")]
+    [SerializeField] float groundCheckOffset = -0.6f;
+    [SerializeField] LayerMask groundCheck;
+    [SerializeField] float normalGravity = 1f;
+    [SerializeField] float fallGravity = 2f;
+
     [Header("States")]
     [SerializeField] bool isGrounded;
+    [SerializeField] bool isJumping;
+    [SerializeField] bool canMove = true;
 
+
+    [Header("AbilityStates")]
+    [SerializeField] bool canJump;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
+        if (canMove) MovePlayer();
 
+        GroundCheck();
 
+        if(isGrounded && isJumping == true)
+        {
+            isJumping = false;
+        }
+
+        if (isGrounded)
+        {
+            rb2d.gravityScale = normalGravity;
+        }
+
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && !isGrounded)
+        {
+            ChangeGravity(fallGravity);
+            Debug.Log("I am let go!!!!");
+        }
+    }
+
+    private void ChangeGravity(float gravity)
+    {
+        rb2d.gravityScale = gravity;
     }
 
     private void MovePlayer()
     {
-        Vector2 movement = new Vector2(transform.position.x + movementSpeed * Time.deltaTime,transform.position.y);
-        Debug.Log(movement);
+        Vector2 movement = new Vector2(transform.position.x + movementSpeed * Time.deltaTime, transform.position.y);
+        //Debug.Log(movement);
         transform.position = movement;
     }
 
     public void Jump()
     {
+        if (isGrounded && canJump)
+        {
+            isJumping = true;
+            Vector2 jumpVector = new Vector2(0, jumpheight);
+            rb2d.AddForce(jumpVector, ForceMode2D.Force);
+        }
+    }
 
+    public void GroundCheck()
+    {
+        Vector2 groundCheckOffsetVector = new Vector2(transform.position.x, transform.position.y + groundCheckOffset);
+        isGrounded = Physics2D.Linecast(transform.position, groundCheckOffsetVector, groundCheck);
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Vector3 debugVector = new Vector3(transform.position.x, transform.position.y + groundCheckOffset, 0);
+    //    Debug.DrawLine(transform.position, debugVector, Color.red, groundCheck);
+    //}
+
+    public void TriggerMovement(bool state)
+    {
+        canMove = state;
     }
 }
+
